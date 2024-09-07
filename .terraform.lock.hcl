a70466101b61f04d8817236585a17bf317661c2f
Celyne <dianakydd2@gmail.com>
2024-09-07 22:45:20 +0300

    update

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -4,6 +4,7 @@
 provider "registry.terraform.io/hashicorp/aws" {
   version = "4.0.0"
   hashes = [
+    "h1:G0toIzBkhRr/UNRdksvnIyPXnGT9nH0A7gWmu93I1Eg=",
     "h1:XjvC5UMR+bRj8Rt9T2VhJdryCfe6lxDaXUSOKFviV5c=",
     "h1:wHydn6CP2wkxUfcq4nRw2NdCc4+rERltXaNZ97U0zAo=",
     "zh:02937cb37860b022e7d996726e7584ca23904baf7852d266f2dd7891ee088ae4",
---
+++ cdk-terraform-config/main.tf
@@ -0,0 +1,23 @@
+terraform { 
+  cloud { 
+    
+    organization = "celyne-project-19" 
+
+    workspaces { 
+      name = "s3-webapp-workspace" 
+    } 
+  } 
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+module "s3-webapp" {
+  source  = "app.terraform.io/celyne-project-19/s3-webapp/aws"
+  version = "1.0.0"
+  name    = "webapp-bucket"
+  region  = "eu-central-1"
+  prefix  = "cdk"
+}
